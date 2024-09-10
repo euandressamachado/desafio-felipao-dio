c69d06305f15d805b4c6a76c73bee39aed8e62b5
euandressamachado <andressamachadosouza@gail.com>
2024-09-10 15:38:16 -0300

    atualização do desafio-dio

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
-console.log("oi")
+//# 1 Desafio Classificador de nível de Herói
+
+let heroi = "Ragnar"
+let xp = 10001
+
+if (xp <= 1000){
+    nivel = "Ferro"
+}else if (xp > 1000 && xp <= 2000){
+    nivel = "Bronze"
+}else if (xp > 2000 && xp <= 5000){
+    nivel = "Prata"
+}else if (xp > 5000 && xp <= 7000){
+    nivel = "Ouro"
+}else if (xp > 7000 && xp <= 8000){
+    nivel = "Platina"
+}else if (xp > 8000 && xp <= 9000){
+    nivel = "Ascendente"
+}else if (xp > 9000 && xp <= 10000){
+    nivel = "Imortal"
+}else (xp >= 100001);{
+    nivel = "Radiante"
+}
+
+console.log("O Herói de nome " + heroi + "está no nível " + nivel)
